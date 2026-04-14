5793a5b464b5019aa761ab4617aa48da79c44806
whussein-9 <whussein@saintpeters.edu>
2026-04-14 16:41:13 +0000

    fix app crash

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,17 +3,18 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Home from "@/pages/Home";
-import HowItWorks from "@/pages/HowItWorks";
-import FeaturesPage from "@/pages/FeaturesPage";
-import Pricing from "@/pages/Pricing";
-import Industries from "@/pages/Industries";
-import Resources from "@/pages/Resources";
-import BlogPost from "@/pages/BlogPost";
-import TrialPage from "@/pages/TrialPage";
-import ContactPage from "@/pages/ContactPage";
-import DemoPage from "@/pages/DemoPage";
-import NotFound from "@/pages/not-found";
+
+import Home from "./pages/Home";
+import HowItWorks from "./pages/HowItWorks";
+import FeaturesPage from "./pages/FeaturesPage";
+import Pricing from "./pages/Pricing";
+import Industries from "./pages/Industries";
+import Resources from "./pages/Resources";
+import BlogPost from "./pages/BlogPost";
+import TrialPage from "./pages/TrialPage";
+import ContactPage from "./pages/ContactPage";
+import DemoPage from "./pages/DemoPage";
+import NotFound from "./pages/not-found";
 
 function Router() {
   return (
